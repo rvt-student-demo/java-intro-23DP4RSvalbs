f65c7b59b17dbe0ba05df34c42c59518e00c43b4
23DP4RSvalbs <166586314+23DP4RSvalbs@users.noreply.github.com>
2024-09-20 10:46:46 +0000

    Simple calculator

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -1,12 +1,21 @@
 package lv.rvt;
-
+import java.util.Scanner;
 public class App 
 {
     public static void main( String[] args )
     {
-        int cipars1= 8;
-        int cipars2 = 2;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Write the first number");
 
+        int cipars1 = Integer.valueOf(scanner.nextLine());
+        System.out.println("Write the second number");
+        int cipars2 = Integer.valueOf(scanner.nextLine());
+
+    
+
+
+
+        
         int sask = cipars1+cipars2;
         int atn = cipars1-cipars2;
         int reiz = cipars1*cipars2;
