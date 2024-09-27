c3b044b6e7b9762550308e8f0a2e33dfb3ec773e
23DP4RSvalbs <166586314+23DP4RSvalbs@users.noreply.github.com>
2024-09-27 10:15:27 +0000

    Larger Than or Equal To

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -1,17 +1,24 @@
 package lv.rvt;
 import java.util.Scanner;
 public class App 
-{
+{ 
     public static void main( String[] args )
     {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Give speed:");
+        System.out.println("Give me the first number:");
 
         int cipars1 = Integer.valueOf(scanner.nextLine());
+        System.out.println("Give me the second number:");
+        int cipars2 = Integer.valueOf(scanner.nextLine());
 
 
-        if (cipars1 > 120 ) {
-            System.out.println("Speeding ticket!");
+        if (cipars1 > cipars2 ) {
+            System.out.println("Greater number is:"+cipars1);
+        }  else if (cipars1 < cipars2) {
+            System.out.println("Greater number is:"+cipars2);
+        }  else {
+                System.out.println("The number are equal!");
+            }
         }
-    }    
-}
+}    
+
