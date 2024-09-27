ea6cf153d59f8a4bdca28df9b504f85ed59915df
23DP4RSvalbs <166586314+23DP4RSvalbs@users.noreply.github.com>
2024-09-27 09:38:29 +0000

    Speeding Ticket

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -5,25 +5,13 @@ public class App
     public static void main( String[] args )
     {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Write the first number");
+        System.out.println("Give speed:");
 
         int cipars1 = Integer.valueOf(scanner.nextLine());
-        System.out.println("Write the second number");
-        int cipars2 = Integer.valueOf(scanner.nextLine());
-
-    
 
 
-
-        
-        int sask = cipars1+cipars2;
-        int atn = cipars1-cipars2;
-        int reiz = cipars1*cipars2;
-        int dal = cipars1/cipars2;
-
-        System.out.println(cipars1+"+"+cipars2+"="+sask);
-        System.out.println(cipars1+"+"+cipars2+"="+atn);
-        System.out.println(cipars1+"+"+cipars2+"="+reiz);
-        System.out.println(cipars1+"+"+cipars2+"="+dal);
-    }
+        if (cipars1 > 120 ) {
+            System.out.println("Speeding ticket!");
+        }
+    }    
 }
