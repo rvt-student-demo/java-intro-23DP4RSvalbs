191ee601d76087b6ca9fdd7c68bd571363acd2ae
23DP4RSvalbs <166586314+23DP4RSvalbs@users.noreply.github.com>
2024-09-27 10:40:53 +0000

    Grades and Points

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -5,17 +5,27 @@ public class App
     public static void main( String[] args )
     {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("How old are you?");
+        System.out.println("Give point [0-100]:");
 
         int cipars1 = Integer.valueOf(scanner.nextLine());
 
 
-        if (cipars1 <18 ) {
-            System.out.println("You are not an adult");
-        }  else if (cipars1 >18) {
-            System.out.println("You are an adult");
+        if (cipars1 <= 0 ) {
+            System.out.println("impossible!");
+        }  else if (cipars1 >= 0 && cipars1 <= 49) {
+            System.out.println("failed");
+        }  else if (cipars1 >= 50 && cipars1 <= 59) {
+            System.out.println("1");
+        }  else if (cipars1 >= 60 && cipars1 <= 69) {
+            System.out.println("2");
+        }  else if (cipars1 >= 70 && cipars1 <= 79) {
+            System.out.println("3");
+        }  else if (cipars1 >= 80 && cipars1 <= 89) {
+            System.out.println("4");
+        }  else if (cipars1 >= 90 && cipars1 <= 100) {
+            System.out.println("5");
         }  else {
-                System.out.println("You are a adult");
+                System.out.println("Incredible");
             }
         }
 }    
