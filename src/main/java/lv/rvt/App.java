92b6cb8a8705adf59adfd93d0f98ebdbf0c5fe08
23DP4RSvalbs <166586314+23DP4RSvalbs@users.noreply.github.com>
2024-10-10 11:14:23 +0000

    Number and sum of numbers

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -5,28 +5,35 @@ public class App
     public static void main( String[] args )
     {
         Scanner scanner = new Scanner(System.in);
-        double tax = 0;
+        int n3 = 0;
+        int n2 = 0;    
 
-        System.out.print("Value of the gift? ");
-        double giftValue = scanner.nextDouble();
 
-        if (giftValue < 5000) {
-            System.out.println("No tax!");
-        } else if (giftValue >= 5000 && giftValue < 25000) {
-            tax = 100 + (giftValue - 5000) * 0.08;
-        } else if (giftValue >= 25000 && giftValue < 55000) {
-            tax = 1700 + (giftValue - 25000) * 0.10;
-        } else if (giftValue >= 55000 && giftValue < 200000) {
-            tax = 4700 + (giftValue - 55000) * 0.12;
-        } else if (giftValue >= 200000 && giftValue < 1000000) {
-            tax = 22100 + (giftValue - 200000) * 0.15;
-        } else {
-            tax = 142100 + (giftValue - 1000000) * 0.17;
+    while (true) {
+        System.out.println("Give me a number:");
+        int n1 = Integer.valueOf(scanner.nextLine());
+
+        if (n1 != 0) {
+            n3 = n3 + 1;
+            n2 = n2+ n1;
+        }
+        if (n1 == 0) {
+            System.out.println("Number of numbers:"+ n3);
+            System.out.println("Sum of the numbers:"+ n2);
+    
+            break;
         }
 
-        System.out.println(String.format("Tax: %.2f€", tax));
-       
         
+
+    } 
+
+    
+        
+
+
+
+
     }
 }
 
