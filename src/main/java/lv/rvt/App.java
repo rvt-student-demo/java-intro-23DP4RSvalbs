5b0db2fb736ba974cd7688559cf9c6d70dc2d16c
23DP4RSvalbs <166586314+23DP4RSvalbs@users.noreply.github.com>
2024-10-10 11:50:46 +0000

    Counting

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -5,23 +5,11 @@ public class App
     public static void main( String[] args )
     {
         Scanner scanner = new Scanner(System.in);
-        int n3 = 0;
-        int n2 = 0;    
-
-
-    while (true) {
-        System.out.println("Give me a number:");
-        int n1 = Integer.valueOf(scanner.nextLine());
-
-        if (n1 != 0) {
-            n3 = n3 + 1;
-            n2 = n2+ n1;
+        int n2 = Integer.valueOf(scanner.nextLine());  
+        for (int i = 0; i <= n2; i++) {
+            System.out.println(i);
         }
-        if (n1 == 0) {
-            System.out.println("Number of numbers:"+ n3);
-            System.out.println("Sum of the numbers:"+ n2);
-    
-            break;
+
         }
 
         
@@ -30,11 +18,5 @@ public class App
 
     
         
-
-
-
-
-    }
-}
 
 
