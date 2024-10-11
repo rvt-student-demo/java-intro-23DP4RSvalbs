d5bb8710536799e31de152f3c3645aa178f50088
23DP4RSvalbs <166586314+23DP4RSvalbs@users.noreply.github.com>
2024-10-11 11:10:12 +0000

    In a hole in the ground

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -2,65 +2,37 @@ package lv.rvt;
 import java.util.Scanner;
 public class App {
     public static void main( String[] args ){
-
-
-
-
-        
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Give a numbers:");
-        int ns = -1;
-        int sum = 1;
-        int avg = 0;
-        int p = 0;
-        int n = 0;
-
-        while (true){
 
 
-            int num = scanner.nextInt();
-            sum = sum + num;
-            ns++;
-            
-
-
-            if (ns != 0){
-                avg = sum/ns;
-            
-
-
+        System.out.println("How many times?");
+        int n = scanner.nextInt();
+            for (int i =1;i<=n;++i){
+                printText();
             }
-            
-            if (num == -1){
-                System.out.println("Thx! Bye!");
-                System.out.println("Sum:"+sum);
-                System.out.println("Numbers:"+ns++);
-                System.out.println("Average:"+avg);
-                System.out.println("Even:"+p);
-                System.out.println("Odd:"+n);
+    }
 
-                
-                break;
-            
-            }
+    public static void printText() {
+        
 
-            if (num % 2 ==0){
-                p = p +1;
-            } else {
-                n = n + 1;
-                }
-            
-            
+            System.out.println("In a hole in the ground there lived a method");
         
         
-                
-            
-            
-        }
-
+        
+        
 
 
     }
+
+        
+        
+        
+
+
+
+
+
+    
 
 
 
