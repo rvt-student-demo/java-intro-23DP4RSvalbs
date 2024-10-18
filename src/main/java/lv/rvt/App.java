adf58599ab77afd7619102be11a08fc0f1facd5a
23DP4RSvalbs <166586314+23DP4RSvalbs@users.noreply.github.com>
2024-10-18 10:53:49 +0000

    Printing a rectangle

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -1,17 +1,28 @@
 package lv.rvt;
 import java.util.Scanner;
 public class App {
-    public static void printsSquare(int size) {
-        for(int i = 1;i <=size; i++) {
-            System.out.print("****");
-            System.out.println("");
+    public static void printsRectangle(int width, int height) {
 
+        for(int i = 1;i <=width; i++)  {
+            System.out.print("*");
 
-        }
+                for(int j =1;j <= height; j++) {
+                System.out.println("");
+                }
+            }
 
+                    
+
+                
+    
+            
+                
+        
+        
+        
     }
     public static void main( String[] args ){
-        printsSquare(4);
+        printsRectangle(17,3);
 
         
 
