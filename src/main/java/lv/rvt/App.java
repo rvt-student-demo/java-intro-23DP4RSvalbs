7ac5af4e33811e91f115a5633efdc2b7432ab5a0
23DP4RSvalbs <166586314+23DP4RSvalbs@users.noreply.github.com>
2024-10-25 10:18:19 +0000

    Star sign (4 parts)

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -1,45 +1,54 @@
 package lv.rvt;
-import java.util.Scanner;
+
 public class App {
-    public static void printsRectangle(int width, int height) {
 
-        for(int i = 1;i <=width; i++)  {
+    public static void main(String[] args){
+        printSquare(4);
+        System.out.println();
+        printRectangle(17, 3);
+        System.out.println();
+        printTriangle(4);
+    }
+
+    public static void printStars(int number) {
+
+        for (int i=0;i<number;i++) {
             System.out.print("*");
+        }
+        System.out.println();
+    }
 
-                for(int j =1;j <= height; j++) {
-                System.out.println("");
-                }
+        public static void printSquare(int size) {
+            for (int i =0; i <size;i++){
+                printStars(size);
             }
 
-                    
+        }
 
-                
-    
-            
-                
-        
-        
-        
-    }
-    public static void main( String[] args ){
-        printsRectangle(17,3);
+        public static void printRectangle(int width, int height){
+            for (int i=0; i<height;i++){
+                printStars(width);
 
-        
+            }
+        }
 
-    }
 
-    
-
-    
+        public static void printTriangle(int size){
+            for (int i=1;i<=size;i++){
+                printStars(i);
+            }
+        }
 
 
 
 
 
 
-}
+
+
 
     
+}  
     
 
             
