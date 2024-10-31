d45b2c4a4f6658d65ba0118a2523ffee13198f14
23DP4RSvalbs <166586314+23DP4RSvalbs@users.noreply.github.com>
2024-10-31 12:32:43 +0000

    Chapter 60 Programming Exercise 3

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -4,24 +4,23 @@ public class App {
 
     public static void main ( String[] args )
     {
-        int[] val = {13, -4, 82, 17}; 
-        int[] twice = new int[4]; 
+        int[] valA = {13, -22, 82, 17}; 
+        int[] valB = {-12, 24, -79, -13};
+        int[] sum = {0, 0, 0, 0};
+        
+        
+        sum[0] = valA[0] + valB[0];
+        sum[1] = valA[1] + valB[1];
+        sum[2] = valA[2] + valB[2];
+        sum[3] = valA[3] + valB[3];
 
-        System.out.println("Original Array: " 
-            + val[0] + " " + val[1] + " " + val[2] + " " + val[3] );
-
-        for (int i = 0; i < 4; i++) { 
-            twice[i] = val[i] * 2; 
-        }
-
-        System.out.println("New Array: " 
-            + twice[0] + " " + twice[1] + " " + twice[2] + " " + twice[3] );
+        System.out.println("sum: " 
+            + sum[0] + " " + sum[1] + " " + sum[2] + " " + sum[3]);
     }
   
 }
 
         
-
 
 
 
