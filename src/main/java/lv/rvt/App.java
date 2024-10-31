2ba73920cd67d6c0462cf72315cd699d592dacb4
23DP4RSvalbs <166586314+23DP4RSvalbs@users.noreply.github.com>
2024-10-31 13:26:12 +0000

    Chapter 60 Programming Exercise 5

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -4,30 +4,27 @@ public class App {
 
     public static void main ( String[] args )
     {
-        int[] valA = {13, -22, 82, 17}; 
-        int[] valB = {-12, 24, -79, -13};
+        int[] val = {0, 1, 2, 3}; 
+        int temp;
+
+        System.out.println("Original Array: " 
+            + val[0] + " " + val[1] + " " + val[2] + " " + val[3]);
 
         
-        
-        valB[0] = 25 - valA[0]; 
-        valB[1] = 25 - valA[1]; 
-        valB[2] = 25 - valA[2]; 
-        valB[3] = 25 - valA[3];
+        temp = val[0];
+        val[0] = val[3];
+        val[3] = temp;
 
-        System.out.println("valA: " 
-        + valA[0] + " " + valA[1] + " " + valA[2] + " " + valA[3]);
+        temp = val[1];
+        val[1] = val[2];
+        val[2] = temp;
 
-    System.out.println("valB: " 
-        + valB[0] + " " + valB[1] + " " + valB[2] + " " + valB[3]);
-
-    System.out.println("sum:  " 
-        + (valA[0] + valB[0]) + " " + (valA[1] + valB[1]) + " " 
-        + (valA[2] + valB[2]) + " " + (valA[3] + valB[3]));
+        System.out.println("Reversed Array: " 
+            + val[0] + " " + val[1] + " " + val[2] + " " + val[3]);
     }
   
 }
 
-            
             
 
         
