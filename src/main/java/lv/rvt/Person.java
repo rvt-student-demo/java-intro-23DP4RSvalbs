2e90c92a283cf9f0bd9f45a4fe586ad2b42627a3
23DP4RSvalbs <166586314+23DP4RSvalbs@users.noreply.github.com>
2024-11-08 11:51:24 +0000

    klases darbs

---
+++ src/main/java/lv/rvt/Person.java
@@ -0,0 +1,13 @@
+package lv.rvt;
+import java.util.*;
+
+
+public class Person {
+    private String name;
+    private int age;
+
+    public Person(String initialName) {
+        this.age = 0;
+        this.name = initialName;
+    }
+}
