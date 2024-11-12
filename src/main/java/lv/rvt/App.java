ff99b9ff78a271151b2d56cb5834108cbab68546
23DP4RSvalbs <166586314+23DP4RSvalbs@users.noreply.github.com>
2024-11-12 13:20:55 +0000

    Decreasing counter 1

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -1,53 +1,22 @@
 package lv.rvt;
 import java.util.*;
-public class Main {
+
+import lv.DecreasingCounter;
+public class App {
     public static void main(String[] args) {
+        DecreasingCounter counter = new DecreasingCounter(10);
+
+        counter.printValue();
+
+        counter.decrement();
+        counter.printValue();
+
+        counter.decrement();
+        counter.printValue();
         
-        Scanner scanner = new Scanner(System.in);
-        ArrayList<String> names = new ArrayList<>();
-        ArrayList<Integer> birthYears = new ArrayList<>();
-
-     
-        while (true) {
-            System.out.print("Enter name and birth year (name,year) or press enter to finish: ");
-            String input = scanner.nextLine();
-            
-         
-            if (input.isEmpty()) {
-                break;
-            }
-            
-      
-            String[] parts = input.split(",");
-            String name = parts[0];
-            int year = Integer.parseInt(parts[1]);
-
-            names.add(name);
-            birthYears.add(year);
-        }
-
-       
-        String longestName = "";
-        for (String name : names) {
-            if (name.length() > longestName.length()) {
-                longestName = name;
-            }
-        }
-
-     
-        int sumOfYears = 0;
-        for (int year : birthYears) {
-            sumOfYears += year;
-        }
-        double averageBirthYear = (double) sumOfYears / birthYears.size();
-
-       
-        System.out.println("Longest name: " + longestName);
-        System.out.println("Average of the birth years: " + averageBirthYear);
     }
+
 }
-
-
 
 
 
