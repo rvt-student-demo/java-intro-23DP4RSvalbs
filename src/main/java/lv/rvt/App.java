2626b3e0477a46a64a4bb745ceb25ae8080c6f57
23DP4RSvalbs <166586314+23DP4RSvalbs@users.noreply.github.com>
2024-11-12 13:23:24 +0000

    Decreasing counter 2

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -4,8 +4,11 @@ import java.util.*;
 import lv.DecreasingCounter;
 public class App {
     public static void main(String[] args) {
-        DecreasingCounter counter = new DecreasingCounter(10);
+        DecreasingCounter counter = new DecreasingCounter(2);
 
+        counter.printValue();
+
+        counter.decrement();
         counter.printValue();
 
         counter.decrement();
