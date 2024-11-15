d0607b8fde290ab14f92503bcb61fb11facee4db
23DP4RSvalbs <166586314+23DP4RSvalbs@users.noreply.github.com>
2024-11-15 11:45:09 +0000

    Payment Card 2

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -4,9 +4,18 @@ public class App {
     public static void main(String[] args) {
         PaymentCard card = new PaymentCard(50);
         System.out.println(card);
+
+        card.eatAffordably();
+        System.out.println(card);
+
+        card.eatHeartily();
+        card.eatAffordably();
+        System.out.println(card);
+
     }
        
 }
+
 
 
 
--- src/main/java/lv/rvt/PaymentCard.java
+++ src/main/java/lv/rvt/PaymentCard.java
@@ -6,8 +6,22 @@ public class PaymentCard {
     public PaymentCard(double openingBalance) {
         this.balance = openingBalance;
     }
+
+    public void eatAffordably() {
+        if (this.balance >= 2.60) { // Only deduct if there's enough balance
+            this.balance -= 2.60;
+        }
+    }
+
+    public void eatHeartily() {
+        if (this.balance >= 4.60) { // Only deduct if there's enough balance
+            this.balance -= 4.60;
+        }
+    }
+
     @Override
     public String toString() {
         return "The card has a balance of " + this.balance + " euros";
     }
 }
+
