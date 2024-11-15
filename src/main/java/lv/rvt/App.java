84e7bbfc4e48705026107ef24972bfb00ee4d40a
23DP4RSvalbs <166586314+23DP4RSvalbs@users.noreply.github.com>
2024-11-15 11:29:19 +0000

    Statistics 3

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -2,17 +2,32 @@ package lv.rvt;
 import java.util.*;
 public class App {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         Statistics statistics = new Statistics();
-        statistics.addNumber(3);
-        statistics.addNumber(5);
-        statistics.addNumber(1);
-        statistics.addNumber(2);
-        System.out.println("Count: " + statistics.getCount());
-        System.out.println("Sum: "+ statistics.sum());
-        System.out.println("Average: "+ statistics.average());
-    }
+        System.out.println("Enter numbers:");
 
+        while (true) {
+            
+            int number = Integer.parseInt(scanner.nextLine());
+
+            
+            if (number == -1) {
+                break;
+            }
+
+           
+            statistics.addNumber(number);
+        }
+
+        
+        System.out.println("Sum: " + statistics.sum());
+    }
+       
 }
+
+
+
+
 
 
 
--- src/main/java/lv/rvt/Statistics.java
+++ src/main/java/lv/rvt/Statistics.java
@@ -1,38 +1,30 @@
 package lv.rvt;
 
+import java.util.Scanner;
+
 public class Statistics {
+    Scanner scanner = new Scanner(System.in);
     private int count;
     private int sum;
 
     public Statistics() {
-        
         this.count = 0;
         this.sum = 0;
     }
-
+    
     public void addNumber(int number) {
         
         this.count++;
         this.sum += number;
     }
 
-    public int getCount() {
-
-        return this.count;
-    }
-
     public int sum() {
         return this.sum;
-
     }
-
-    public double average() {
-        return this.count == 0 ? 0 : (double) this.sum / this.count;
-
-    }
-
 
 }       
+
+
 
 
 
