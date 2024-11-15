bd65241cb358f91562cdc3dab4c0a42967deff32
23DP4RSvalbs <166586314+23DP4RSvalbs@users.noreply.github.com>
2024-11-15 11:06:31 +0000

    Statistics 2

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -8,6 +8,8 @@ public class App {
         statistics.addNumber(1);
         statistics.addNumber(2);
         System.out.println("Count: " + statistics.getCount());
+        System.out.println("Sum: "+ statistics.sum());
+        System.out.println("Average: "+ statistics.average());
     }
 
 }
