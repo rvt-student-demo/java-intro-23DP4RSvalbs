d65dd78865a7068c498af75ce1062b52198c88d4
23DP4RSvalbs <166586314+23DP4RSvalbs@users.noreply.github.com>
2024-11-22 11:46:54 +0000

    One Minute

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -1,25 +1,18 @@
 package lv.rvt;
 public class App {
     public static void main(String[] args) {
-        PaymentCard paulsCard = new PaymentCard(20);
-        PaymentCard mattsCard = new PaymentCard(30);
-        
+        Timer timer = new Timer();
 
-        paulsCard.eatHeartily();
-        System.out.println("Paul: "+paulsCard);
-        mattsCard.eatAffordably();
-        System.out.println("Matt: "+mattsCard);
+    while (true) {
+    System.out.println(timer);
+    timer.advance();
 
-        paulsCard.addMoney(20);
-        System.out.println("Paul: "+paulsCard);
-        mattsCard.eatHeartily();
-        System.out.println("Matt: "+mattsCard);
+    try {
+        Thread.sleep(10);
+    } catch (Exception e) {
 
-        paulsCard.eatAffordably();
-        paulsCard.eatAffordably();
-        System.out.println("Paul: "+paulsCard);
-        mattsCard.addMoney(50);
-        System.out.println("Matt: "+mattsCard);
+    }
+}
 
 
     }
@@ -30,9 +23,5 @@ public class App {
 
 
 
-
-
-    
-        
 
 
