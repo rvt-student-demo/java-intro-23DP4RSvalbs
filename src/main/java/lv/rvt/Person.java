c4f16441a895ac6fb31789f0cb1c62b3e1732bc3
23DP4RSvalbs <166586314+23DP4RSvalbs@users.noreply.github.com>
2024-12-06 07:46:18 +0000

    Klases darbs

--- src/main/java/lv/rvt/Person.java
+++ src/main/java/lv/rvt/Person.java
@@ -3,12 +3,14 @@ package lv.rvt;
 
 public class Person {
     private String name;
-
+    private int age;
+    private int height;
     private int weight;
 
     public Person(String name, int age, int height, int weight) {
         this.name = name;
-
+        this.age = age;
+        this.height = height;
         this.weight = weight;
         
     }
--- src/main/java/lv/rvt/PersonManager.java
+++ src/main/java/lv/rvt/PersonManager.java
@@ -4,4 +4,5 @@ import java.util.ArrayList;
 
 public class PersonManager {
     public static ArrayList<Person> getPersonList() throws Exception
+    
 }
