fe9f725f60b9d310cdc6fb03f7813f34bd44958a
Rolands Svalbs <166586314+23DP4RSvalbs@users.noreply.github.com>
2025-01-10 13:49:01 +0000

    Exercise 1 — Immutable Box

---
+++ src/main/java/lv/rvt/Box.java
@@ -0,0 +1,29 @@
+package lv.rvt;
+
+public class Box {
+
+    private double width;
+    private double height;
+    private double length;
+
+ 
+    public Box(double width, double length, double height){
+        this.width = width;
+        this.height = height;
+        this.length = length;
+    }
+
+    public Box(double side) {
+        this(side, side, side);
+    }
+
+
+    public double volume() {
+        return width * height * length;
+    }
+
+
+    public double area() {
+        return 2 * (width * height + height * length + length * width);
+    }
+}
