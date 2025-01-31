e275841a6814abadc64f65cfd823b29b995119f7
Rolands Svalbs <166586314+23DP4RSvalbs@users.noreply.github.com>
2025-01-31 11:18:13 +0000

    Student's toString

--- src/main/java/lv/rvt/Student.java
+++ src/main/java/lv/rvt/Student.java
@@ -16,12 +16,16 @@ public class Student extends Person {
         this.credits++;
     }
 
+    @Override
+    public String toString(){
+        return super.toString() + "\nStudy credits " + this.credits;
+
+    }
+
     public static void main(String[] args) {
         Student ollie = new Student("Ollie", "6381 Hollywood Blvd. Los Angeles 90028");
-
-        System.out.println(ollie);
-        System.out.println("Study credits " + ollie.credits());
-        ollie.study();
-        System.out.println("Study credits " + ollie.credits());
+    System.out.println(ollie);
+    ollie.study();
+    System.out.println(ollie);
     }
 }
