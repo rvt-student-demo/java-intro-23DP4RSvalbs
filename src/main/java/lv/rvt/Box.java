34db9bc67e4a99f785b6b1c597ea18766736737b
Rolands Svalbs <166586314+23DP4RSvalbs@users.noreply.github.com>
2025-02-14 12:24:31 +0000

    Box

--- src/main/java/lv/rvt/Box.java
+++ src/main/java/lv/rvt/Box.java
@@ -1,80 +1,28 @@
 package lv.rvt;
 
+import java.util.ArrayList;
+
 public class Box {
+    private double maxCapacity;
+    private ArrayList<Packable> items;
+    private double currentWeight;
 
-    private double width;
-    private double height;
-    private double length;
-
-
-    public Box(double width, double height, double length) {
-        this.width = width;
-        this.height = height;
-        this.length = length;
-    }
-    public Box(double side) {
-        this(side, side, side);
+    public Box(double maxCapacity) {
+        this.maxCapacity = maxCapacity;
+        this.items = new ArrayList<>();
+        this.currentWeight = 0;
     }
 
-    public Box(Box oldBox) {
-        this(oldBox.width, oldBox.height, oldBox.length);
+    public void add(Packable item) {
+        double newWeight = this.currentWeight + item.weight();
+        if (newWeight <= maxCapacity) {
+            items.add(item);
+            currentWeight = newWeight;  
+        }
     }
 
-
-    public double volume() {
-        return width * height * length;
-    }
-
-
-    public double area() {
-        return 2 * (width * height + height * length + length * width);
-    }
-
-
-    private double faceArea() {
-        return width * height;
-    }
-
-    private double topArea() {
-        return width * length;
-    }
-
-
-    private double sideArea() {
-        return height * length;
-    }
-
-    public double width() {
-        return width;
-    }
-
-
-    public double height() {
-        return height;
-    }
-
-
-    public double length() {
-        return length;
-    }
-    public Box biggerBox(Box oldBox) {
-        return new Box(
-            1.25 * oldBox.width(),
-            1.25 * oldBox.height(),
-            1.25 * oldBox.length()
-        );
-    }
-
-    public Box smallerBox(Box oldBox) {
-        return new Box(
-            0.75 * oldBox.width(),
-            0.75 * oldBox.height(),
-            0.75 * oldBox.length()
-        );
+    @Override
+    public String toString() {
+        return "Box: " + items.size() + " items, total weight " + currentWeight + " kg";
     }
 }
-
-
-
-
-
